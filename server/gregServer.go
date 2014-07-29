bab47d2eb80bedb9f182d8a832d1ca42d5096cfa
Sam Jantz <sjantz@riotgames.com>
2014-07-29 16:59:48 -0500

    Extracted Service into an interface and it's own file. implemented pollServices more fully implemented register endpoint.

--- server/gregServer.go
+++ server/gregServer.go
@@ -4,45 +4,66 @@ import (
     "fmt"
     "net/http"
     "time"
+    "encoding/json"
+    "io/ioutil"
+    "log"
   )
-
-type Service struct {
-  Name string `json:"Name"`
-  Ip string `json:"Ip"`
-  Port int `json:"Port"`
-}
-func (s *Service) FullAddress() string{
-  return fmt.Sprintf("%s:%d", s.Ip, s.Port)
-}
-func (s *Service) String() string {
-  return fmt.Sprintf("%s<%s>", s.Name, s.FullAddress())
-}
-
-var services []Service
 
 func Start(port int){
   fmt.Printf("Starting greg on port %d\n", port)
+  setupRestEndpoints()
+
+  serverChannel := make(chan bool)
+  startRegistry(port, serverChannel)
+  <-serverChannel
+}
+
+func startRegistry(port int, serverChannel chan bool){
+  go func() {
+    http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+    serverChannel<-true
+  }()
+  go func() {
+    pollServices()
+    serverChannel<-true
+  }()
+}
+
+func setupRestEndpoints(){
   http.HandleFunc("/register", registerCallback)
   http.HandleFunc("/uptime", uptimeCallback)
   http.HandleFunc("/services", servicesCallback)
   http.HandleFunc("/service", serviceLookupCallback)
-
-  serverChannel := make(chan bool)
-  go func() {
-    http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
-    serverChannel <- true
-  }()
-  pollServices()
-  <-serverChannel
 }
 
-func registerCallback(writer http.ResponseWriter, r *http.Request){ fmt.Fprintf(writer, "<html><body><h1>Not Yet Implemented</h1></body></html>") }
+var services []Servicer
+
+func registerCallback(writer http.ResponseWriter, r *http.Request){
+  defer r.Body.Close()
+  body, err := ioutil.ReadAll(r.Body)
+  if(err != nil){
+    log.Fatal(err)
+  }
+
+  var service Service
+  malformedError := json.Unmarshal(body, &service)
+  if(malformedError != nil){
+    log.Printf("ERR: Malformed JSON %s", string(body))
+  }
+  services = append(services, service)
+}
 func uptimeCallback(writer http.ResponseWriter, r *http.Request){ fmt.Fprintf(writer, "<html><body><h1>Not Yet Implemented</h1></body></html>") }
 func servicesCallback(writer http.ResponseWriter, r *http.Request){ fmt.Fprintf(writer, "<html><body><h1>Not Yet Implemented</h1></body></html>") }
 func serviceLookupCallback(writer http.ResponseWriter, r *http.Request){ fmt.Fprintf(writer, "<html><body><h1>Not Yet Implemented</h1></body></html>") }
+
 func pollServices(){
   for true {
     fmt.Printf("Polling %d registered services\n", len(services))
-    time.Sleep(time.Minute)
+    for index, service := range services {
+      if(!service.IsHealthy()){ //remove the service from the list.
+        services = services[:index+copy(services[index:], services[index+1:])]
+      }
+    }
+    time.Sleep(time.Second * 10)
   }
 }
---
+++ server/service.go
@@ -0,0 +1,35 @@
+package server
+
+import (
+  "fmt"
+  "log"
+  "net/http"
+)
+
+type Servicer interface {
+  FullAddress() string
+  String() string
+  IsHealthy() bool
+}
+
+type Service struct {
+  Name string `json:"Name"`
+  Ip string `json:"Ip"`
+  Port int `json:"Port"`
+}
+func (s Service) FullAddress() string{
+  return fmt.Sprintf("%s:%d", s.Ip, s.Port)
+}
+func (s Service) String() string {
+  return fmt.Sprintf("%s<%s>", s.Name, s.FullAddress())
+}
+
+func (s Service) IsHealthy() bool {
+  _, err := http.Get(fmt.Sprintf("http://%s:%d/healthCheck", s.Ip, s.Port))
+  if(err != nil){
+    log.Printf("ERR: %s", s.String())
+    return false
+  }
+  log.Printf("OK : %s", s.String())
+  return true
+}
